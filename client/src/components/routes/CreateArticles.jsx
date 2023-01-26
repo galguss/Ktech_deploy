8ca0a1be7606b039b50b7a261e5cca5133d436ca
galguss <gal.gus12@gmail.com>
2023-01-26 21:05:28 +0200

    Handling creating a question #1

--- client/src/components/routes/CreateArticles.jsx
+++ client/src/components/routes/CreateArticles.jsx
@@ -24,6 +24,9 @@ function CreateArticles({isLogged}){
                 const response = await fetch(URL, 
                     {
                         method: 'POST',
+                        headers:{
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        },
                         body: formData
                     });
                     const data = await response.json();
